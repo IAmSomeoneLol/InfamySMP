e719fe593c31e5833d3dbda4a571a30d6fee0b75
Nel <alenkobalenko007@gmail.com>
2026-06-14 02:44:33 +0200

    inittial

--- src/main/kotlin/com/enxivity/infamy/listeners/DeathListener.kt
+++ src/main/kotlin/com/enxivity/infamy/listeners/DeathListener.kt
@@ -3,6 +3,9 @@ package com.enxivity.infamy.listeners
 import com.enxivity.infamy.InfamySMP
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextDecoration
+import org.bukkit.Bukkit
+import org.bukkit.Sound
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -22,10 +25,20 @@ class DeathListener(private val plugin: InfamySMP) : Listener {
         if (victimRep >= 21) {
             val bossBottle = plugin.itemManager.createBossBottle()
             val droppedItem = victim.world.dropItemNaturally(victim.location, bossBottle)
-            droppedItem.isInvulnerable = true // Native MC protection layer
+            droppedItem.isInvulnerable = true
+
+            // Feature: Global Announcement
+            if (plugin.config.getBoolean("settings.boss-bottle-announce", true)) {
+                Bukkit.broadcast(Component.text("The Most Infamous Player has fallen! The Core of Infamy has been dropped!", NamedTextColor.DARK_RED, TextDecoration.BOLD))
+            }
+            // Feature: Global Dragon Sound
+            if (plugin.config.getBoolean("settings.boss-bottle-sound", true)) {
+                for (onlinePlayer in Bukkit.getOnlinePlayers()) {
+                    onlinePlayer.playSound(onlinePlayer.location, Sound.ENTITY_ENDER_DRAGON_GROWL, 1.0f, 1.0f)
+                }
+            }
         }
 
-        // Deduct victim's standard points
         val loseAmount = when {
             victimRep >= 21 -> 3
             victimRep >= 11 -> 2
@@ -37,7 +50,7 @@ class DeathListener(private val plugin: InfamySMP) : Listener {
         if (killer != null && killer.uniqueId != victim.uniqueId) {
             val now = System.currentTimeMillis()
             val logs = recentKills.computeIfAbsent(killer.uniqueId) { mutableListOf() }
-            logs.removeIf { now - it > 1800000 } // Clear out records past 30 mins
+            logs.removeIf { now - it > 1800000 }
 
             var awardPoints = 1
             if (plugin.teamManager.areTeammates(killer.uniqueId, victim.uniqueId)) {
@@ -61,14 +74,12 @@ class DeathListener(private val plugin: InfamySMP) : Listener {
                     droppedItem.isInvulnerable = true
                 } else {
                     val killerRep = plugin.infamyManager.getRawReputation(killer)
-                    // If config is false, enforce the Level 20 bottle cap manually
                     val newRep = (killerRep + awardPoints).coerceAtMost(if (killerRep >= 21) 21 else 20)
                     plugin.infamyManager.setReputation(killer, newRep)
                 }
             }
         }
 
-        // Invisibility hiding death records
         if (victimRep >= 15 && plugin.config.getBoolean("infamy_abilities.bad_fortune.enabled", true)) {
             if (victim.hasPotionEffect(org.bukkit.potion.PotionEffectType.INVISIBILITY)) {
                 event.deathMessage(null)
